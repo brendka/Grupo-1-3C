29f68b04ea8ca6409974cd1b4cbce79884c329e1
brendka <161617088+brendka@users.noreply.github.com>
2024-10-08 09:38:09 -0300

    grupu 1

--- scrpt.js
+++ scrpt.js
@@ -1,1 +1,31 @@
 const caixaprincipal = document.querySelector('.caixa-principal');
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto- resultado');
+const lista = [item, item2]
+const perguntas = [
+{
+    enunciado: "insira o enunciadoda pergunta",
+    alternativa: ["alternativa1","alternativa2"]
+},
+{
+enunciado:"insira o enunciado da Pergunta2",
+alternativa: ["alternativa1","alternativa2"],
+},
+{
+    enunciado:"insira o enunciado da pergunta3",
+    alternativa: ["alternativa1","alternativa2"]
+}
+];
+let atual = 0;
+let perguntaAtual;
+function mostraPerguntas(){
+    perguntaAtual = perguntas[atual];
+}
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+function mostraPerguntas(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
