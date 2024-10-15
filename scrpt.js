b1972690cda883053337457f871b1515908b0dfd
brendka <161617088+brendka@users.noreply.github.com>
2024-10-15 09:43:06 -0300

    grupo 1

--- scrpt.js
+++ scrpt.js
@@ -1,5 +1,4 @@
-const caixaprincipal = document.querySelector('.caixa-principal');
-const caixaprincipal = document.querySelector('.caixa-principal');
+const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
